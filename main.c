c95307ece76b1121f74baf9c128da54ba44616e8
Carlos Luis Lobato Romero <lr19047@ues.edu.sv>
2021-09-03 18:09:04 -0600

    Inicialize el repositorio

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: qwerty
+ *
+ * Created on 3 de septiembre de 2021, 18:06
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
