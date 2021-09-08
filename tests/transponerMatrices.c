7369ceef6c7db25c1052c632f23bd59ec360a24d
joel <fuentesjoel723@gmail.com>
2021-09-08 16:06:51 -0600

    tercer test

--- tests/transponerMatrices.c
+++ tests/transponerMatrices.c
@@ -19,14 +19,19 @@ int clean_suite(void) {
 
 void testTransponerMatrices() {
     
-    int *matriz[2][3]={{1,2,3},{4,5,6}};
+    int matriz[2][3]={1,2,3,4,5,6};
    
-    int filas;
-    int columnas;
+    int filas=2;
+    int columnas=3;
     int* result = transponerMatrices(matriz, filas, columnas);
-    if (1 /*check result*/) {
-        CU_ASSERT(0);
-    }
+    
+    CU_ASSERT_EQUAL(*(result + 0 *2+ 1), 4);
+    CU_ASSERT_EQUAL(*(result + 1 *2+ 1), 5);
+    CU_ASSERT_EQUAL(*(result + 0 *2+ 0), 1);
+    CU_ASSERT_EQUAL(*(result + 2 *2+ 0), 3);
+    
+    
+    
 }
 
 int main() {
